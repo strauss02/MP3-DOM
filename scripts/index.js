770be2dddc67a509e7acc134007378f91c7b14d0
Ido Strauss <strauss02@hotmail.co.il>
2021-09-17 18:14:17 +0300

    add play button to every song

--- scripts/index.js
+++ scripts/index.js
@@ -30,8 +30,9 @@ function createSongElement({ id, title, album, artist, duration, coverArt }) {
     let spanAlbum = createElement("span", [album], ["data-cell", "song-album"])
     let spanArtist = createElement("span", [artist], ["data-cell", "song-artist"])
     let spanDuration = createElement("span", [convertSecondsToMinutes(duration)], ["data-cell", "song-duration"])
+    let spanPlay = createElement("a", ["Play"], ["play-button"], { href: "#" })
 
-    const children = [spanWithImg, spanTitle, spanAlbum, spanArtist, spanDuration]
+    const children = [spanWithImg, spanTitle, spanAlbum, spanArtist, spanDuration, spanPlay]
     const classes = ["song-data-container"]
     const attrs = { onclick: `playSong(${id})` }
 
@@ -115,6 +116,8 @@ function convertSecondsToMinutes(time) {
 }
 
 function renderLists(songs, playlists) {
+    let songsList = document.getElementById("songs-container")
+    clearElement(songsList)
     const sortedSongs = songs.sort(sortTitlesAlphabetically)
 
     for (song of sortedSongs) {
@@ -126,7 +129,6 @@ function renderLists(songs, playlists) {
             duration: song.duration,
             coverArt: song.coverArt,
         })
-        let songsList = document.getElementById("songs-container")
         songsList.append(songElement)
     }
 
@@ -141,6 +143,8 @@ function renderLists(songs, playlists) {
         let playlistList = document.getElementById("playlists-container")
         playlistList.append(playlistElement)
     }
+
+    styleEverySecondRow(document.getElementById("songs-container"))
 }
 
 function showSongs() {
@@ -171,28 +175,14 @@ function sortNameAlphabetically(a, b) {
 }
 
 renderLists(player.songs, player.playlists)
-styleEverySecondRow(document.getElementById("songs-container"))
 
 //Handle form
 
-function handleForm(event) {
-    const allInputs = document.querySelectorAll("input")
-    console.log(allInputs)
+function addSong(title, album, artist, duration, id = getVacantId(player.songs), coverImage) {
+    assertStringNotEmpty(title, album, artist)
+    assertIsNumber(id)
+    assertIdNotUsed(id, player.songs)
 
-    const newSongTitle = allInputs[2].value
-    const newSongArtist = allInputs[3].value
-    const newSongAlbum = allInputs[4].value
-    const newSongduration = allInputs[5].value
-    const newSongId = 999
-    const newSongImage = allInputs[7].value
-
-    addSong(newSongTitle, newSongAlbum, newSongArtist, newSongduration, newSongId, newSongImage)
-    console.log(player.songs)
-
-    renderLists(player.songs, player.playlists)
-}
-
-function addSong(title, album, artist, duration, id, coverImage) {
     let newSong = {
         id: id,
         title: title,
@@ -202,7 +192,63 @@ function addSong(title, album, artist, duration, id, coverImage) {
         coverArt: coverImage,
     }
     player.songs.push(newSong)
+    renderLists(player.songs, player.playlists)
     return newSong.id
 }
 
-/************************ Utility Functions *********************************/
+function handleForm(event) {
+    const allInputs = document.querySelectorAll("input")
+    console.log(allInputs)
+
+    const newSongTitle = allInputs[2].value
+    const newSongArtist = allInputs[3].value
+    const newSongAlbum = allInputs[4].value
+    const newSongduration = allInputs[5].value
+    const newSongImage = allInputs[7].value
+
+    addSong(newSongTitle, newSongAlbum, newSongArtist, newSongduration, "", newSongImage)
+    console.log(player.songs)
+
+    renderLists(player.songs, player.playlists)
+}
+/********************************* Utility Functions *********************************/
+
+//gets number (i) and goes through all the songs / playlists to see if anyone has it. if not, it is considered avaliable.
+function getVacantId(array) {
+    mainLoop: for (let i = 1; i <= array.length + 1; i++) {
+        for (let item of array) {
+            if (item.id === i) {
+                continue mainLoop
+            }
+        }
+        return i
+    }
+}
+
+function assertStringNotEmpty(...args) {
+    for (let arg of args) {
+        if (typeof arg === "string" && arg.length != 0) {
+            continue
+        } else throw new Error(`Waahh! You must enter a valid string. you entered: ${arg}`)
+    }
+}
+
+function assertIdNotUsed(id, array) {
+    for (let item of array) {
+        if (id === item.id) {
+            throw new Error(`Whoops! seems like that ID is already in use. ID entered: ${id}`)
+        }
+    }
+}
+
+function assertIsNumber(arg) {
+    if (typeof arg != "number") {
+        throw new Error(`Whoopa! looks like you didn't enter a number. you entered: ${arg}`)
+    }
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
